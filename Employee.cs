8fbf56f1af734c416c1632004b964e4b5cc173ad
girishsj11 <59494641+girishsj11@users.noreply.github.com>
2020-09-23 20:57:21 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeDetails
+{
+    class Employee
+    {
+        public string company_name = "LTTS";
+        string Fname;
+        string Lname;
+        int psno;
+
+
+        public Employee(string FirstName,string LastName,int PSNo )
+        {
+            Fname = FirstName;
+            Lname = LastName;
+            psno = PSNo;
+        }
+
+        string Employee_EmailID()
+        {
+            return (Fname + Lname + '@' + company_name + ".com");
+        }
+
+        int Employee_PSnumber()
+        {
+            return psno;
+        }
+
+        public void GetFullDetailsOfEmployee()
+        {
+            string FullName = Fname + Lname;
+            string EmailID = Employee_EmailID();
+            int PsNo = Employee_PSnumber();
+
+            Console.WriteLine("Employee FullName is :- {0}", FullName);
+            Console.WriteLine("Employee Email ID is :- {0}", EmailID);
+            Console.WriteLine("Employee PSNo is :- {0} ", PsNo);
+            Console.WriteLine();
+
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeDetails;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to main program !!! \n");
+            Console.WriteLine("Specify the number of employees details you want to store here : ");
+            
+            string Number = Console.ReadLine();
+            int num = Convert.ToInt32(Number);
+            Console.WriteLine();
+            if (num >= 1)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine("Enter Employee_{0} FirstName is:",i+1);
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("Enter Employee_{0} LastName is:",i+1);
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Enter Employee_{0} PS Number is:",i+1);
+                    string PS = Console.ReadLine();
+                    int PSNumber = Convert.ToInt32(PS);
+
+                    Employee emp1 = new Employee(FirstName, LastName, PSNumber);
+                    Console.WriteLine();
+                    Console.WriteLine("Employee_{0} full details is below : \n", i);
+                    emp1.GetFullDetailsOfEmployee();
+                    Console.WriteLine();
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("The entered value of count for the Employee details is less what program accepts");
+                Console.WriteLine("Please do Re-run the program & enter value of count more than 1 ");
+            }
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
